b2b9726040b7a7f03234a9a78c097f91a91ca291
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-02 17:39:43 -0400

    Add list of candidates to home page

--- election.js
+++ election.js
@@ -2,4 +2,21 @@ $(document).ready(function() {
 
   // Imagination!
 
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(data){
+
+    for (var i = 0; i < data.candidates.length; i++) {
+      candidate = data.candidates[i];
+      var cInfo = candidate.name + ': ' + candidate.votes + ' votes.';
+      var item = $('<li>').html(cInfo);
+      $('#candidate-list').append(item);
+    }
+
+
+
+  });
+
 });
